56dbcdc54ed824f8f11c581e486ce3b7c54a8805
AqsaSaleem0 <aqsasaleem233@gmail.com>
2026-04-22 23:15:13 +0500

    variable added

--- app/core/config.py
+++ app/core/config.py
@@ -7,7 +7,9 @@ BASE_URL = os.getenv('BASE_URL', 'http://localhost:8000')
 CURRENCY_CODE = os.getenv('CURRENCY_CODE', 'USD')
 CURRENCY_SYMBOL = os.getenv('CURRENCY_SYMBOL', '$')
 WEBHOOK_SECRET = os.getenv('WEBHOOK_SECRET')
-if not WEBHOOK_SECRET:
+
+# Allow testing without WEBHOOK_SECRET
+if not WEBHOOK_SECRET and not os.getenv('TESTING'):
     raise EnvironmentError("CRITICAL: WEBHOOK_SECRET must be set as an environment variable.")
 
 
